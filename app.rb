673a51ba19e9eaad2bf7267420690371a07fb2d7
Nathan Hadzariga <nhadzariga@ubuntu.com>
2013-09-19 11:24:13 -0300

    Add basic functionality and bootstrap

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'ramaze'
+
+class TestApp < Ramaze::Controller
+  
+  engine :erubis
+  
+  def index
+  end
+  
+  def options
+    str = 'OPTIONS:'
+    str += '<br>views: ' + Ramaze.options.views.to_s
+    str += '<br>publics: ' + Ramaze.options.publics.to_s
+    str += '<br>layouts: ' + Ramaze.options.layouts.to_s
+    str += '<br>roots: ' + Ramaze.options.roots.to_s
+    str += '<br>helpers: ' + Ramaze::HelpersHelper.options.paths.to_s
+  end
+  
+end
+
+Ramaze.start :port => 8080
